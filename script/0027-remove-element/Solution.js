da61ee28971872be78b1e0a3262e2b1fc9861bd7
Lucas <lucas.prudente.2004@gmail.com>
2024-11-07 14:50:43 -0300

    Add solutions and description for exercise: 26 - Remove Element

---
+++ script/0027-remove-element/Solution.js
@@ -0,0 +1,18 @@
+// Return the number of elements in nums which are not equal to val
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+var removeElement = function(nums, val) {
+  for(let i = 0; i < nums.length; i++){
+      if(nums[i] === val){
+          nums.splice(i, 1);
+          i--;
+      }
+  }
+
+  return nums.length;
+};
