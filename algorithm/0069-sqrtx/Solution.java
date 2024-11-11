b94e951632c62fd20bfd13e930110bcb1ccfd8f1
Lucas <lucas.prudente.2004@gmail.com>
2024-11-11 17:17:04 -0300

    Add solutions and description for exercise: 69 - Sqrt(x)

---
+++ algorithm/0069-sqrtx/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    // Return the square root of x rounded down to the nearest integer
+    public int mySqrt(int x) {
+        long sqrt = x;
+
+        while (sqrt * sqrt > x) {
+            sqrt /= 2;
+        }
+
+        while (sqrt * sqrt <= x) {
+            sqrt++;
+        }
+
+        return (int) sqrt - 1;
+    }
+}
---
+++ script/0069-sqrtx/Solution.js
@@ -0,0 +1,20 @@
+// Return the square root of x rounded down to the nearest integer
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+var mySqrt = function (x) {
+  let sqrt = 0;
+
+  while (sqrt * sqrt > x) {
+      sqrt /= 2;
+  }
+
+  while (sqrt * sqrt <= x) {
+      sqrt++;
+  }
+
+  return sqrt - 1;
+};
