b28d5173119a7fc1f6733a6d50e2ae14957070a0
Lucas <lucas.prudente.2004@gmail.com>
2024-11-29 11:46:22 -0300

    Add solutions and description for exercise: 263 - Ugly Number

---
+++ algorithm/0263-ugly-number/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    // Return if n is an ugly number
+    public boolean isUgly(int n) {
+        if (n <= 0) { return false; }
+        if (n <= 3 || n == 5) { return true; }
+
+        if (n % 2 == 0) { return isUgly(n / 2); }
+        if (n % 3 == 0) { return isUgly(n / 3); }
+        if (n % 5 == 0) { return isUgly(n / 5); }
+
+        return false;
+    }
+}
