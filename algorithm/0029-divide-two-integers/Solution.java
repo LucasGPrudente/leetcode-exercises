49a474a0f56689276c04c141d9ebec63df6bb3d5
Lucas <lucas.prudente.2004@gmail.com>
2024-12-16 15:52:10 -0300

    Add solutions and description for exercise: 29 - Divide Two Integers

---
+++ algorithm/0029-divide-two-integers/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    // Return the quotient after dividing dividend by divisor
+    public int divide(int dividend, int divisor) {
+        if (divisor == -1) {
+            return (dividend == Integer.MIN_VALUE) ? Integer.MAX_VALUE : dividend * -1;
+        }
+
+        if (divisor == 1) {
+            return dividend;
+        }
+
+        boolean isNegative = dividend < 0 != divisor < 0;
+
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        while (absDividend >= absDivisor) {
+            long tempDivisor = absDivisor, multiple = 1;
+
+            while (absDividend >= (tempDivisor << 1)) {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+
+            absDividend -= tempDivisor;
+            quotient += multiple;
+        }
+
+        return isNegative ? -quotient : quotient;
+    }
+}
---
+++ script/0029-divide-two-integers/Solution.js
@@ -0,0 +1,41 @@
+// Return the quotient after dividing dividend by divisor
+
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+
+var divide = function (dividend, divisor) {
+    const max = Math.pow(2, 31) - 1;
+    const min = Math.pow(2, 31) * -1;
+
+    if (divisor == -1) {
+        return (dividend == min) ? max : dividend * -1;
+    }
+
+    if (divisor == 1) {
+        return dividend;
+    }
+
+    const isNegative = dividend < 0 != divisor < 0;
+    let result = 0;
+
+    dividend = Math.abs(dividend);
+    divisor = Math.abs(divisor);
+
+    while (dividend >= divisor) {
+        let multiple = 1;
+        let val = divisor;
+
+        while (dividend >= val + val) {
+            val += val;
+            multiple += multiple;
+        }
+
+        dividend -= val;
+        result += multiple;
+    }
+
+    return isNegative ? -result : result;
+};
